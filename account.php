924ebc77a332045e93b7854e2794282987be0351
awbrunet <awbrunet@uvm.edu>
2014-11-27 01:22:47 -0500

    Account page now recognizes user

--- account.php
+++ account.php
@@ -5,7 +5,33 @@ include "top.php";
 ?>
 
 <article id="main">
-	<p> <a href = 'login.php'>Login</a></p>
+	<?php
+	require_once('../bin/myDatabase.php');
+
+    $dbUserName = 'awbrunet_reader';
+    $whichPass = "r"; //flag for which one to use.
+    $dbName =  'AWBRUNET_UVM_Courses';
+    $thisDatabase = new myDatabase($dbUserName, $whichPass, $dbName);
+
+    $query = 'SELECT fldLogStatus FROM tblUser WHERE fldLogStatus=1';
+    $checkLogin = $thisDatabase->select($query);
+
+
+	if(empty($checkLogin)){
+		print "<p> <a href = 'login.php'>Login</a></p>";
+	}
+	else{
+		$query = 'SELECT fldEmail FROM tblUser WHERE fldLogStatus=1';
+		$email = $thisDatabase->select($query);
+
+		print "<p>Logged in as ";
+		foreach ($email as $result){
+
+			print $result['fldEmail'];
+		}
+	}
+
+	?>
 </article>
 
 <?php include "footer.php"; ?>
