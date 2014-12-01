a788bdef213920e3143a16123dd703f4b1dd836c
awbrunet <awbrunet@uvm.edu>
2014-11-30 20:38:03 -0500

    Adding to home

--- index.php
+++ index.php
@@ -5,7 +5,13 @@ include "top.php";
 ?>
 
 <article id="main">
-<p>home</p>
+<h3>What is myGlutenFree?</h3>
+<br>
+<p>In recent years, there's been a new 'trend'; a fad diet called 'gluten-free'. Many gluten-free eaters don't even know what gluten <i>is</i>.
+<br>
+
+
+</p>
 </article>
 
 <?php include "footer.php"; ?>
